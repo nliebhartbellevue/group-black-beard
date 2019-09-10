49b4cd046ff82407caa726edc5e1b457662b1a77
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2019-09-10 13:05:14 -0500

    board categories

--- jeopardy-ui/index.js
+++ jeopardy-ui/index.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+import "bootstrap";
+import "@fortawesome/fontawesome-free";
+import "./scss/main.scss";
+
+const categories = [
+  {
+    id: 1,
+    name: "Lexical Structure"
+  },
+  {
+    id: 2,
+    name: "Objects"
+  },
+  {
+    id: 3,
+    name: "Arrays"
+  },
+  {
+    id: 4,
+    name: "Functions"
+  },
+  {
+    id: 5,
+    name: "Handling Events"
+  }
+];
+
+const cat = () => {
+  let output = "";
+  var catTile = document.getElementById("categories");
+  categories.forEach(category => {
+    output += `
+        <div class="tile">${category.name}</div>
+      `;
+  });
+
+  catTile.innerHTML = output;
+};
+
+cat();
